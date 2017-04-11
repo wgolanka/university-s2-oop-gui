516ea052956c965a39e11ed7895226cf920ffc57
Wiktoria Golanka <wiktoria.golanka@gmail.com>
2017-04-11 22:23:20 +0200

    Calculator -done, Hailstone - done

--- GUI3_GW_S15565/src/zad2/Calc.java
+++ GUI3_GW_S15565/src/zad2/Calc.java
@@ -7,23 +7,51 @@
 package zad2;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.HashMap;
 
 public class Calc 
 {
-	BigDecimal bd;
-	String plus;
-
-	public String doCalc(String string) 
-	{
-		
-		bd = new BigDecimal(string);
-		
-		return string;
-		
-	}
+	BigDecimal numA;
+	BigDecimal numB;
+	HashMap<String, BigMath> map;
 	
-//	public String substract(String string )
-//	{
-//		
-//	}
+	interface BigMath
+	{
+		BigDecimal operation(BigDecimal a, BigDecimal b);
+	}
+
+	public String doCalc(String string)
+	{
+		String[] array = string.split("\\s+");
+		map = new HashMap<String, BigMath>();
+		
+		try
+		{
+			numA = new BigDecimal(Integer.valueOf(array[0]));
+			numB = new BigDecimal(Integer.valueOf(array[2]));	
+			String operator = array[1];
+			
+			System.out.println("number A: " + numA);
+			System.out.println("Operator: " + operator);
+			System.out.println("number B: " + numB);
+			
+			BigMath addition = (numA, numB) -> numA.add(numB, MathContext.DECIMAL32);		
+			BigMath substraction = (numA, numB) -> numA.subtract(numB, MathContext.DECIMAL32);
+			BigMath multiply = (numA, numB) -> numA.multiply(numB, MathContext.DECIMAL32);
+			BigMath divide = (numA, numB) -> numA.divide(numB, MathContext.DECIMAL32);
+		 	
+			map.put("+", addition);
+			map.put("-", substraction);
+			map.put("*", multiply);
+			map.put("/", divide);
+			
+			return String.valueOf(map.get(operator).operation(numA, numB));
+		
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+			return "Invalid command to calc";
+		}			
+	}
 }  
